6e3ec2ec86628707634a5abc5bcd2640fc1bb102
Matheus Souza <m7hsouza@gmail.com>
2023-01-08 13:25:34 -0400

    feat(Route): add new route to create new post

--- server/router/postsRouter.js
+++ server/router/postsRouter.js
@@ -8,4 +8,10 @@ router.get('/posts', async function(request, response) {
 	response.json(posts);
 });
 
+router.post('/posts', async function(request, response) {
+	const post = request.body;
+	const newPost = await postsService.savePost(post);
+	response.json(newPost);
+});
+
 module.exports = router;
