f272266e24c943e14687b696e662f4df3457e1ec
Matheus Souza <m7hsouza@gmail.com>
2023-01-08 13:08:36 -0400

    feat(Data): add savePost method

--- server/data/postsData.js
+++ server/data/postsData.js
@@ -2,4 +2,12 @@ const database = require('../infra/database');
 
 exports.getPosts = function () {
 	return database.query('select * from blog.posts')
-}
+};
+
+exports.savePost = function(post) {
+	return database.one('insert into blog.posts (title, content) values ($1, $2) returning *', [post.title, post.content]);
+};
+
+exports.deletePost = function (id) {
+	return database.none('delete from blog.posts where id = $1', [id]);
+};
