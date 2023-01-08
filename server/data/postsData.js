bdf0a6e744889d80594ccfdb0236d8681e2d2e15
Matheus Souza <m7hsouza@gmail.com>
2023-01-08 13:47:27 -0400

    feat(Data): :sparkles: add updatePost and getPost methods

--- server/data/postsData.js
+++ server/data/postsData.js
@@ -4,10 +4,18 @@ exports.getPosts = function () {
 	return database.query('select * from blog.posts')
 };
 
+exports.getPost = function (id) {
+	return database.oneOrNone('select * from blog.posts where id = $1', [id]);
+};
+
 exports.savePost = function(post) {
 	return database.one('insert into blog.posts (title, content) values ($1, $2) returning *', [post.title, post.content]);
 };
 
 exports.deletePost = function (id) {
 	return database.none('delete from blog.posts where id = $1', [id]);
+};
+
+exports.updatePost = function (id, post) {
+	return database.none('update blog.posts set title = $1, content = $2 where id = $3', [post.title, post.content, id]);
 };
