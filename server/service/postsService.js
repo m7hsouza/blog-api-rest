71862ffe7aec147f86edd5b7f8bd735f8ab6f16d
Matheus Souza <m7hsouza@gmail.com>
2023-01-08 13:09:05 -0400

    feat(Service): add savePost method

--- server/service/postsService.js
+++ server/service/postsService.js
@@ -2,4 +2,12 @@ const postsData = require('../data/postsData');
 
 exports.getPosts = function () {
 	return postsData.getPosts();
-}
+};
+
+exports.savePost = function (post) {
+	return postsData.savePost(post);
+};
+
+exports.deletePost = function (id) {
+	return postsData.deletePost(id);
+};
