e2d18ac87f1ce5f5be1758ae0a442ca24108b814
Matheus Souza <m7hsouza@gmail.com>
2023-01-08 13:10:01 -0400

    refactor(Test): update get posts test

--- server/test/posts.test.js
+++ server/test/posts.test.js
@@ -1,10 +1,28 @@
 const axios = require('axios');
+const crypto = require('crypto');
+const postsService = require('../service/postsService');
+
+const generate = function () {
+	return crypto.randomBytes(20).toString('hex');
+}
+
+const request = function (method, url, data) {
+	return axios({ url, method, data });
+}
 
 test('Should get posts', async function () {
-	const response = await axios({
-		url: 'http://localhost:3000/posts',
-		method: 'get'
-	});
+	// given - dado que
+	const post1 = await postsService.savePost({ title: generate(), content: generate() });
+	const post2 = await postsService.savePost({ title: generate(), content: generate() });
+	const post3 = await postsService.savePost({ title: generate(), content: generate() });
+
+	// when - quando acontecer
+	const response = await request('get', 'http://localhost:3000/posts');
 	const posts = response.data;
-	expect(posts).toHaveLength(6);
+
+	// then - então
+	expect(posts).toHaveLength(3);
+	await postsService.deletePost(post1.id);
+	await postsService.deletePost(post2.id);
+	await postsService.deletePost(post3.id);
 });
