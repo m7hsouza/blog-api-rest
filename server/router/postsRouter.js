b5b7b949b6eeceb15d1b35d53f627e0ad0cac40a
Matheus Souza <m7hsouza@gmail.com>
2023-01-18 21:46:09 -0400

    refactor(Router): add status code success

--- server/router/postsRouter.js
+++ server/router/postsRouter.js
@@ -11,18 +11,18 @@ router.get('/posts', async function(request, response) {
 router.post('/posts', async function(request, response) {
 	const post = request.body;
 	const newPost = await postsService.savePost(post);
-	response.json(newPost);
+	response.status(201).json(newPost);
 });
 
 router.put('/posts/:id', async function(request, response) {
 	const post = request.body;
 	await postsService.updatePost(request.params.id, post);
-	response.end();
+	response.status(204).end();
 });
 
 router.delete('/posts/:id', async function(request, response) {
 	await postsService.deletePost(request.params.id);
-	response.end();
+	response.status(204).end();
 });
 
 module.exports = router;
--- server/test/posts.test.js
+++ server/test/posts.test.js
@@ -21,6 +21,7 @@ test('Should get posts', async function () {
 	const posts = response.data;
 
 	// then - então
+	expect(response.status).toBe(200);
 	expect(posts).toHaveLength(3);
 	await postsService.deletePost(post1.id);
 	await postsService.deletePost(post2.id);
@@ -30,6 +31,7 @@ test('Should get posts', async function () {
 test('Should save a post', async function () {
 	const data = { title: generate(), content: generate() };
 	const response = await request('post', 'http://localhost:3000/posts', data);
+	expect(response.status).toBe(201);
 	const post = response.data;
 	expect(post.title).toBe(data.title);
 	await postsService.deletePost(post.id);
@@ -39,7 +41,8 @@ test('Should update a post', async function () {
 	const post = await postsService.savePost({ title: generate(), content: generate() });
 	post.title = generate();
 	post.content = generate();
-	await request('put', `http://localhost:3000/posts/${post.id}`, post);
+	const response = await request('put', `http://localhost:3000/posts/${post.id}`, post);
+	expect(response.status).toBe(204);
 	const updatedPost = await postsService.getPost(post.id);
 	expect(updatedPost.title).toBe(post.title);
 	expect(updatedPost.content).toBe(post.content);
@@ -48,7 +51,8 @@ test('Should update a post', async function () {
 
 test('Should delete a post', async function () {
 	const post = await postsService.savePost({ title: generate(), content: generate() });
-	await request('delete', `http://localhost:3000/posts/${post.id}`);
+	const response = await request('delete', `http://localhost:3000/posts/${post.id}`);
+	expect(response.status).toBe(204);
 	const posts = await postsService.getPosts();
 	expect(posts).toHaveLength(0);
 });
