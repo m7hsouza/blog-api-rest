05198b1f67959712b8c3df20047e4b7759ff9614
Matheus Souza <m7hsouza@gmail.com>
2023-01-22 07:40:20 -0400

    implementing error handler

--- server/index.js
+++ server/index.js
@@ -2,7 +2,14 @@ const express = require('express');
 
 const app = express();
 app.use(express.json());
-
 app.use('/', require('./router/postsRouter'));
-
+app.use((error, _, response, __) => {
+	if (error.message == 'Post already exists') {
+		return response.status(409).send(error.message);
+	}
+	if (error.message === 'Post not found') {
+		response.status(404).send(error.message);
+	}
+	return response.status(500).send(error.message);
+})
 app.listen(3000);
--- server/router/postsRouter.js
+++ server/router/postsRouter.js
@@ -3,34 +3,38 @@ const postsService = require('../service/postsService');
 
 const router = express.Router();
 
-router.get('/posts', async function(request, response) {
+router.get('/posts', async function(request, response, next) {
 	const posts = await postsService.getPosts();
 	response.json(posts);
 });
 
-router.post('/posts', async function(request, response) {
+router.post('/posts', async function(request, response, next) {
 	try {
 		const post = request.body;
 		const newPost = await postsService.savePost(post);
 		response.status(201).json(newPost);
-	} catch (e) {
-		response.status(409).send(e.message);
+	} catch (error) {
+		next(error);
 	}
 });
 
-router.put('/posts/:id', async function(request, response) {
+router.put('/posts/:id', async function(request, response, next) {
 	try {
 		const post = request.body;
 		await postsService.updatePost(request.params.id, post);
 		response.status(204).end();
-	} catch (e) {
-		response.status(404).send(e.message);
+	} catch (error) {
+		next(error);
 	}
 });
 
-router.delete('/posts/:id', async function(request, response) {
-	await postsService.deletePost(request.params.id);
-	response.status(204).end();
+router.delete('/posts/:id', async function(request, response, next) {
+	try {
+		await postsService.deletePost(request.params.id);
+		response.status(204).end();
+	} catch (error) {
+		next(error);
+	}
 });
 
 module.exports = router;
